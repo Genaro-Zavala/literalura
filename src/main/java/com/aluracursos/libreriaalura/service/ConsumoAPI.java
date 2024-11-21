54279d38e8f0757ef5ef02e7ffad4dd073cfaa53
Genaro Zavala <genaro.zavala@outlook.com>
2024-11-21 16:30:25 -0500

    Carga de archivos

---
+++ src/main/java/com/aluracursos/libreriaalura/service/ConsumoAPI.java
@@ -0,0 +1,41 @@
+package com.aluracursos.libreriaalura.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class ConsumoAPI {
+    public String obtenerDatos(String url){
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+       HttpResponse<String> response = null;
+        //HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //revisar si va las dos lineas siguientes
+        String json = response.body();
+        return json;
+    }
+
+
+
+
+
+}
